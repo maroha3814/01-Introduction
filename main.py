33d06b53a731802807829d03044039ac05824b86
Matt Harrison <mattharrison3814@gmail.com>
2019-08-28 19:20:07 -0400

    Updates Lisence, readme, and finishes assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Matthew Harrison, though I go by Matt!')
+print('Lots of choices, though I always go back to Skyrim. That game is why Im on this path in life and why Im here today.')
+print('The coding aspect. 2 sessions in and Im terrified that I wont be able to meet expectations.')
+print('Finally getting to interact with what I want to do and, in the end, make an actual game with other people. Also getting to learn about level design.')
+print('11991629')
+print('https://github.com/maroha3814')
